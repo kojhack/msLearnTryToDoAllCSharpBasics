59eb42599b2a9e1f5ada21180a3431b9745ac60f
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-05 10:35:53 +0000

    modified

--- Defis/DefiReverseString/DefiReverse/Program.cs
+++ Defis/DefiReverseString/DefiReverse/Program.cs
@@ -1,16 +1,36 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
-string result = "";
+﻿// string pangram = "The quick brown fox jumps over the lazy dog";
+// string result = "";
 
-string[] items = pangram.Split(' ');
-foreach (string item in items)
+// string[] items = pangram.Split(' ');
+// foreach (string item in items)
+// {
+//     char[] itemArray = item.ToCharArray();
+//     Array.Reverse(itemArray);
+//     //result = String.Join(" ", itemArray);
+//     //itemArray = String.Join(" ", itemArray);
+//     foreach (char i in itemArray)
+//     {
+//         Console.Write(i);
+//     }
+//     //Console.Write(itemArray);
+// }
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
 {
-    char[] itemArray = item.ToCharArray();
-    Array.Reverse(itemArray);
-    result = String.Join(" ", itemArray);
-    //itemArray = String.Join(" ", itemArray);
-    
-    Console.Write(itemArray);
-
-
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
 }
 
+//Step 4
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
--- Defis/DefiReverseString/DefiReverse/obj/Debug/net8.0/DefiReverse.AssemblyInfo.cs
+++ Defis/DefiReverseString/DefiReverse/obj/Debug/net8.0/DefiReverse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefiReverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477007c0ba4c7f1728e64c3759be5d1c703206b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a82199e1d7a4fda87d8c11df7b318b39350ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefiReverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefiReverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
