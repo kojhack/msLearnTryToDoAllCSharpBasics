b8a82199e1d7a4fda87d8c11df7b318b39350ea6
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-05 09:55:43 +0000

    new DefiReverse

---
+++ Defis/DefiReverseString/DefiReverse/Program.cs
@@ -0,0 +1,16 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string result = "";
+
+string[] items = pangram.Split(' ');
+foreach (string item in items)
+{
+    char[] itemArray = item.ToCharArray();
+    Array.Reverse(itemArray);
+    result = String.Join(" ", itemArray);
+    //itemArray = String.Join(" ", itemArray);
+    
+    Console.Write(itemArray);
+
+
+}
+
