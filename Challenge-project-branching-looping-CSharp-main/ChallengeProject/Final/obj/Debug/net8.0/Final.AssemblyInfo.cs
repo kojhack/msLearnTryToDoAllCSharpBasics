8dfcabdc89906a659e817bed26e65c744a48f378
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-12 16:26:21 +0000

    modified

--- Challenge-project-branching-looping-CSharp-main/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Challenge-project-branching-looping-CSharp-main/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844dff4f742837dd159f17af81d0d09c3ed1b481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8089da71c5cad7de86947c2a1da13476e9896c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
